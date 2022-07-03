e94ac026831cde11c305cb95e049424636c24174
Jasir Ali <jasirali407@gmail.com>
2022-07-03 15:08:07 +0530

    add comment section to the post and list comments

--- models/post.js
+++ models/post.js
@@ -4,12 +4,16 @@ const postSchema = new mongoose.Schema({
     content:{
         type: String,
         required: true
-    },
-
+    },    
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+     //  include the array of ids of all comments in this post schema itself 
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
 },{
     timestamps: true
 });
